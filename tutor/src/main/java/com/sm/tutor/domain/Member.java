673b29a892797ffc1272a8521663fe95662b0d8d
dsa04156 <dsa04156@naver.com>
2024-07-31 19:33:32 +0900

    feat : db변경

--- tutor/src/main/java/com/sm/tutor/domain/Member.java
+++ tutor/src/main/java/com/sm/tutor/domain/Member.java
@@ -1,65 +1,69 @@
 package com.sm.tutor.domain;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.time.Instant;
 import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "member", schema = "modu_tutor")
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id", nullable = false)
     private Integer id;
 
-    @Size(max = 320)
-    @NotNull
-    @Column(name = "email", nullable = false, length = 320)
-    private String email;
+    @Size(max = 45)
+    @Column(name = "name", length = 45)
+    private String name;
 
-    @Size(max = 512)
-    @NotNull
-    @Column(name = "password", nullable = false, length = 512)
+    @Size(max = 64)
+    @Column(name = "password", length = 64)
     private String password;
 
-    @Size(max = 50)
-    @NotNull
-    @Column(name = "nickname", nullable = false, length = 50)
+    @Size(max = 45)
+    @Column(name = "email", length = 45)
+    private String email;
+
+    @Size(max = 45)
+    @Column(name = "nickname", length = 45)
     private String nickname;
 
-    @Size(max = 50)
-    @NotNull
-    @Column(name = "phone_number", nullable = false, length = 50)
+    @Size(max = 20)
+    @Column(name = "phone_number", length = 20)
     private String phoneNumber;
 
-    @NotNull
-    @Column(name = "sex", nullable = false)
-    private Integer sex;
+    @Size(max = 10)
+    @Column(name = "gender", length = 10)
+    private String gender;
 
-    @NotNull
-    @Column(name = "birth", nullable = false)
+    @Column(name = "birth")
     private LocalDate birth;
 
-    @NotNull
-    @Column(name = "verified_oauth", nullable = false)
-    private Boolean verifiedOauth = false;
+    @Size(max = 45)
+    @Column(name = "verified_oauth", length = 45)
+    private String verifiedOauth;
 
-    @NotNull
-    @Column(name = "lastlogin", nullable = false)
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "lastlogin")
     private Instant lastlogin;
 
-    @NotNull
-    @Column(name = "type", nullable = false)
-    private Integer type;
+    @Size(max = 45)
+    @Column(name = "type", length = 45)
+    private String type;
 
-    @Size(max = 20)
-    @Column(name = "invite_code", length = 20)
+    @Size(max = 45)
+    @Column(name = "invite_code", length = 45)
     private String inviteCode;
 
 }
