0d4abc33a1dd7e3400e9a2b3f1e07e9620cacf6c
Minuk <holmane33@gmail.com>
2024-09-02 17:31:45 +0900

    docs: 강의 리스트 API에 대한 설명 추가

--- tutor/src/main/java/com/sm/tutor/controller/LectureController.java
+++ tutor/src/main/java/com/sm/tutor/controller/LectureController.java
@@ -6,6 +6,10 @@ import com.sm.tutor.service.LectureService;
 import com.sm.tutor.service.LocationService;
 import com.sm.tutor.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import java.util.List;
@@ -101,11 +105,24 @@ public class LectureController {
     return new ResponseEntity<>(lectureDtoPages, HttpStatus.OK);
   }
 
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "OK: 성공", content = {
+          @Content(schema = @Schema(implementation = Page.class))})
+  })
   @Operation(summary = "필터링 + 강의 리스트",
       description = """ 
+          paging에 대해서는 paging API와 동일
+                   \s
+          추가적으로 List<Integer> categoryId, Integer tuitionMaximum, List<Integer> locationId, Integer online, String keyword 을 받아서 이를 토대로 필터링 진행
+                   \s
+          강의 리스트에 대한 정보는 json에서 content에 list 형식으로 저장됩니다.
+                   \s
+          모든 파라미터는 required = false로, 파라미터 값을 반드시 보낼 필요는 없습니다. 파라미터가 없을 경우 NULL 처리됩니다.
+                   \s
+          성공하면 `200 OK` 상태 코드와 강의 리스트에 대한 정보를 반환합니다.
           """)
   @GetMapping("/list")
-  public ResponseEntity<Page<SimpleLectureResponseDto>> getLectureByFilter(@PageableDefault(page = 0, size = 10, sort = {"id"}) Pageable pageable,
+  public ResponseEntity<Page<SimpleLectureResponseDto>> getLectureByFilter(@PageableDefault(page = 0, size = 20, sort = {"id"}) Pageable pageable,
       @RequestParam(required = false) List<Integer> categoryId,
       @RequestParam(required = false) Integer tuitionMaximum,
       @RequestParam(required = false) List<Integer> locationId, @RequestParam(required = false) Integer online,
